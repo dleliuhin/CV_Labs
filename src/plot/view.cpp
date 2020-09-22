9850c63bfd40341cedfaab3b580bfe39bb74b83c
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-09-23 00:34:52 +0300

    Feature. Lab 11.

--- src/plot/view.cpp
+++ src/plot/view.cpp
@@ -14,8 +14,6 @@
 //=======================================================================================
 View::View( const std::string& name, const Config& conf )
 {
-    cv::moveWindow( name, 0, 0 );
-
     if ( conf.main.rotate == 0 )
         _rotate_code = - 1;
 
@@ -48,7 +46,37 @@ void View::plot( const Pack& data )
     if ( _rotate_code != - 1 )
         cv::rotate( img, img, _rotate_code );
 
+    cv::moveWindow( _name, 0, 0 );
     cv::imshow( _name, img );
+
+    cv::Mat gray;
+    cv::cvtColor( img, gray, cv::COLOR_RGB2GRAY );
+    cv::threshold( gray, gray, 0, 255, cv::THRESH_OTSU );
+
+    cv::moveWindow( "Binary", 400, 0 );
+    cv::imshow( "Binary", gray );
+
+    auto element = cv::getStructuringElement( cv::MORPH_RECT, cv::Size( 4, 4 ), cv::Point( 2, 2 ) );
+    cv::morphologyEx( gray, gray, cv::MORPH_DILATE, element, { - 1, - 1 }, 2 );
+
+    cv::moveWindow( "Dilate", 800, 0 );
+    cv::imshow( "Dilate", gray );
+
+    cv::morphologyEx( gray, gray, cv::MORPH_ERODE, element, { - 1, - 1 }, 2 );
+
+    cv::moveWindow( "Erode", 1200, 0 );
+    cv::imshow( "Erode", gray );
+
+    cv::morphologyEx( gray, gray, cv::MORPH_OPEN, element, { - 1, - 1 }, 2 );
+
+    cv::moveWindow( "Open", 0, 500 );
+    cv::imshow( "Open", gray );
+
+    cv::morphologyEx( gray, gray, cv::MORPH_CLOSE, element, { - 1, - 1 }, 2 );
+    cv::bitwise_not( gray, gray );
+
+    cv::moveWindow( "Close", 400, 500 );
+    cv::imshow( "Close", gray );
 
     auto key = cv::waitKey(10);
 
