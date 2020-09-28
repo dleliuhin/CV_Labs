cf2e1d6ac9aa52ea23820580caa283bf6e76b5e3
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-09-28 17:25:17 +0300

    Feature. Lab 10.

--- src/plot/view.cpp
+++ src/plot/view.cpp
@@ -11,11 +11,27 @@
 
 #include "vlog.h"
 
+#include <iostream>
+
+const int alpha_slider_max = 100;
+int denoise_slider {0};
+int noise_slider {0};
+
+cv::Mat dst;
+
+//=======================================================================================
+void on_trackbar( int, void* )
+{
+    cv::moveWindow( "denoised", 700, 0 );
+    cv::imshow( "denoised", dst );
+}
+//=======================================================================================
+
+
 //=======================================================================================
 View::View( const std::string& name, const Config& conf )
+    : _name ( name )
 {
-    cv::moveWindow( name, 0, 0 );
-
     if ( conf.main.rotate == 0 )
         _rotate_code = - 1;
 
@@ -27,6 +43,16 @@ View::View( const std::string& name, const Config& conf )
 
     else if ( conf.main.rotate == 180 )
         _rotate_code = cv::ROTATE_180;
+
+    cv::namedWindow( "denoised", 1 );
+    cv::createTrackbar( "denoise", "denoised",
+                        &denoise_slider, alpha_slider_max,
+                        on_trackbar );
+
+    cv::namedWindow( "denoised", 2 );
+    cv::createTrackbar( "noise", "denoised",
+                        &noise_slider, alpha_slider_max,
+                        on_trackbar );
 }
 //=======================================================================================
 
@@ -43,12 +69,29 @@ void View::plot( const Pack& data )
 
     auto img = cv::imdecode( buf, cv::IMREAD_COLOR );
 
-    cv::resize( img, img, { 400, 400 } );
+    cv::resize( img, img, { 600, 600 } );
 
     if ( _rotate_code != - 1 )
         cv::rotate( img, img, _rotate_code );
 
+    cv::moveWindow( _name, 0, 0 );
     cv::imshow( _name, img );
+
+    auto type = img.type();
+
+    cv::Mat gauss = cv::Mat( img.size(), CV_16SC3 );
+    cv::randn( gauss, cv::Scalar::all(0), cv::Scalar::all( noise_slider ) );
+
+    img.convertTo( img, CV_16SC3 );
+    cv::addWeighted( img, 1.0, gauss, 1.0, 0.0, img );
+
+    img.convertTo( img, type );
+
+    dst = img;
+
+    cv::fastNlMeansDenoisingColored( dst, dst, denoise_slider );
+
+    on_trackbar( denoise_slider, 0 );
 
     auto key = cv::waitKey(10);
 
